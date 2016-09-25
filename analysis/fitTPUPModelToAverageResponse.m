f3e20f5e7c8d206990686082ab015278b390601f
gkaguirre <gkaguirre@me.com>
2016-09-25 10:17:37 -0400

    put TPUP model fitting into a separate function
    Signed-off-by: gkaguirre <gkaguirre@me.com>

---
+++ analysis/fitTPUPModelToAverageResponse.m
@@ -0,0 +1,53 @@
+function [twoComponentFitToData] = fitTPUPModelToAverageResponse(avgPackets)
+
+% Identify the number of (sessions x subjects) and the number of stimulus
+% types from the dimensionality of the avgPackets cell array
+
+NSessionsMerged=size(avgPackets,1);
+NStimTypes=size(avgPackets,2);
+
+% Define a parameter lock matrix, which in this case is empty
+paramLockMatrix = [];
+
+% We will fit each average response as a single stimulus in a packet, so
+% each packet therefore contains a single stimulus instance.
+defaultParamsInfo.nInstances = 1;
+
+% Announce what we are about to do
+fprintf('>> Fitting two-component model to pupil data (TPUP)\n');
+
+% Pre-allocate a structure to hold the fit results
+twoComponentFitToData{NSessionsMerged,NStimTypes}.paramsFit=[];
+twoComponentFitToData{NSessionsMerged,NStimTypes}.fVal=[];
+twoComponentFitToData{NSessionsMerged,NStimTypes}.fitResponse=[];
+
+% Loop over subjects and stimulus types
+% Skipping the attention task for now
+for ss = 1:NSessionsMerged
+    for mm = 1:NStimTypes-1
+        % Update the user
+        fprintf('* Subject, stimulus <strong>%g</strong> , <strong>%g</strong>', ss, mm);
+        fprintf('\n');
+        
+        % Construct the model object
+        temporalFit = tfeTPUP('verbosity','none');
+        
+        % Grab a single packet
+        singlePacket=avgPackets{ss, mm};
+        
+        % Conduct the fit
+        [paramsFit,fVal,modelResponseStruct] = temporalFit.fitResponse(singlePacket, 'defaultParamsInfo', defaultParamsInfo, ...
+            'paramLockMatrix',paramLockMatrix);
+        
+        % Store the fitResponse
+        twoComponentFitToData{ss,mm}.paramsFit=paramsFit;
+        twoComponentFitToData{ss,mm}.fVal=fVal;
+        twoComponentFitToData{ss,mm}.modelResponseStruct=modelResponseStruct;
+        
+        % Clear the object
+        delete(temporalFit);
+        
+    end % loop over subjects
+end % loop over stimuli
+
+end % function
--- analysis/pupilMelanopsinMRIAnalysis.m
+++ analysis/pupilMelanopsinMRIAnalysis.m
@@ -26,10 +26,10 @@ sessDirs = {...
     'MelanopsinMRMaxMelCRF/HERO_mxs1/061016'
     };
 
-%sessDirs = {'MelanopsinMRMaxLMSCRF/HERO_gka1/060616'};
+sessDirs = {'MelanopsinMRMaxLMSCRF/HERO_gka1/060616'};
 % Define which sessions we'd like to merge
 whichSessionsToMerge = {[1], [2], [3], [4], [5], [6], [7], [8 9]};
-%whichSessionsToMerge = {[1]};
+whichSessionsToMerge = {[1]};
 
 % Set some parameters we need
 normalizationTimeSecs = 0.1;
@@ -164,52 +164,7 @@ for ss = 1:NSessionsMerged
 end
 
 %% Fit model to avg packets
-% Insert code here. Average packets are now in avgPackets{ss, mm}, where ss
-% is subject (with merged session), and mm is stimulus type
-
-% Define a parameter lock matrix, which in this case is empty
-paramLockMatrix = [];
-
-% We will fit each average response as a single stimulus in a packet, so
-% each packet therefore contains a single stimulus instamce.
-defaultParamsInfo.nInstances = 1;
-
-% Announce what we are about to do
-fprintf('>> Fitting two-component model to pupil data (TPUP)\n');
-
-% Pre-allocate a structure to hold the fit results
-twoComponentFitToData{NSessionsMerged,NStimTypes}.paramsFit=[];
-twoComponentFitToData{NSessionsMerged,NStimTypes}.fVal=[];
-twoComponentFitToData{NSessionsMerged,NStimTypes}.fitResponse=[];
-
-% Loop over subjects and stimulus types
-% Skipping the attention task for now
-for ss = 1:NSessionsMerged
-    for mm = 1:NStimTypes-1
-        % Update the user
-        fprintf('* Subject, stimulus <strong>%g</strong> , <strong>%g</strong>', ss, mm);
-        fprintf('\n');
-        
-        % Construct the model object
-        temporalFit = tfeTPUP('verbosity','high');
-        
-        % Grab a single packet
-        singlePacket=avgPackets{ss, mm};
-        
-        % Conduct the fit
-        [paramsFit,fVal,modelResponseStruct] = temporalFit.fitResponse(singlePacket, 'defaultParamsInfo', defaultParamsInfo, ...
-            'paramLockMatrix',paramLockMatrix);
-        
-        % Store the fitResponse
-        twoComponentFitToData{ss,mm}.paramsFit=paramsFit;
-        twoComponentFitToData{ss,mm}.fVal=fVal;
-        twoComponentFitToData{ss,mm}.modelResponseStruct=modelResponseStruct;
-        
-        % Clear the object
-        delete(temporalFit);
-        
-    end % loop over subjects
-end % loop over stimuli
+twoComponentFitToData = fitTPUPModelToAverageResponse(avgPackets);
 
 %% Plot the data
 for ss = 1:NSessionsMerged
