e488eed064112d849d70af5e13172719c196a32e
harrison-mcadams <harrison.mcadams@gmail.com>
2016-10-20 20:58:19 -0400

    more comparable simulated data
    -baseline size now has low frequency oscillations
    -packet created now has multiple runs

--- loading/pupilPMEL_makeSimulatedData.m
+++ loading/pupilPMEL_makeSimulatedData.m
@@ -1,3 +1,6 @@
+function [simulatedMergedPacketCellArray] = pupilPMEL_makeSimulatedData()
+
+
 % Make stimulus vector. In this case, just grab an existing stimulus
 % vector
 % from some random subject.
@@ -48,60 +51,60 @@ switch packetCacheBehavior
         error('Please define a legal packetCacheBehavior');
 end
 
-thePacket= mergedPacketCellArray{1,1}{1,1};
-
-
-contrastList = {0.25; 0.50; 1; 2; 4; 8};
-thePacket.stimulus.vector = zeros(1,length(thePacket.stimulus.timebase));
-neuronalResponse = zeros(1,length(thePacket.stimulus.timebase));
-pupilResponse = zeros(1,length(thePacket.stimulus.timebase));
-
-% create pupil response function, which is to convolved with the
-% neuronalResponse. For now, this pupil response function is just a step to
-% 2 for 3 s
-pupilResponseFunction = zeros(1,length(thePacket.stimulus.timebase));
-pupilResponseFunction(101:3000) = -2;
-
-% Create pupil size vector. For now, just a baseline size of 5 mm
-pupilSize = ones(1,length(thePacket.stimulus.timebase));
-pupilSize = 5*pupilSize;
-
-for ii = 1:size(thePacket.stimulus.values,1);
+simulatedMergedPacketCellArray = [];
+for rr = 1:(size(mergedPacketCellArray{1},2))
+    thePacket= mergedPacketCellArray{1}{rr};
     
-    % Find the stimulus onsets so that we can align the data to it. We
-    % do that by finding a [0 1] edge from a difference operator.
-    tmp = diff(thePacket.stimulus.values(ii,:));
-    tmp(tmp < 0) = 0;
-    tmp(tmp > 0) = 1;
     
-    % Check if the very first value is 1, in which case the stim onset is
-    % at the initial value
-    if tmp(1)==1
-        stimOnset=1;
-    else
-        stimOnset = strfind(tmp, [0 1]);
+    contrastList = {0.25; 0.50; 1; 2; 4; 8};
+    thePacket.stimulus.vector = zeros(1,length(thePacket.stimulus.timebase));
+    neuronalResponse = zeros(1,length(thePacket.stimulus.timebase));
+    pupilResponse = zeros(1,length(thePacket.stimulus.timebase));
+    
+    % create pupil response function, which is to convolved with the
+    % neuronalResponse. For now, this pupil response function is just a step to
+    % 2 for 3 s
+    pupilResponseFunction = zeros(1,length(thePacket.stimulus.timebase));
+    pupilResponseFunction(101:3000) = -2;
+    
+    % Create pupil size vector. For now, just a baseline size of 5 mm
+    pupilSize = thePacket.response.metaData.lowFreqComponent;
+    
+    
+    simulatedMergedPacketCellArray{1}{rr} = [];
+    for ii = 1:size(thePacket.stimulus.values,1);
+        
+        % Find the stimulus onsets so that we can align the data to it. We
+        % do that by finding a [0 1] edge from a difference operator.
+        tmp = diff(thePacket.stimulus.values(ii,:));
+        tmp(tmp < 0) = 0;
+        tmp(tmp > 0) = 1;
+        
+        % Check if the very first value is 1, in which case the stim onset is
+        % at the initial value
+        if tmp(1)==1
+            stimOnset=1;
+        else
+            stimOnset = strfind(tmp, [0 1]);
+        end
+        contrast = thePacket.stimulus.metaData.stimTypes(ii);
+        
+        thePacket.stimulus.vector(stimOnset) = contrastList{contrast};
+        neuronalResponse(stimOnset) = log10(thePacket.stimulus.vector(stimOnset))+1;
+        
+        % In this mode, pupil response is proportional to the size at the
+        % beginning of stimulus presentation
+        pupilResponse(stimOnset) = neuronalResponse(stimOnset)/10 * pupilSize(stimOnset);
+        
+        
     end
-    contrast = thePacket.stimulus.metaData.stimTypes(ii);
     
-    thePacket.stimulus.vector(stimOnset) = contrastList{contrast};
-    neuronalResponse(stimOnset) = log10(thePacket.stimulus.vector(stimOnset))+1;
+    pupilMotor = conv(pupilResponse,pupilResponseFunction);
+    pupilMotor = pupilMotor(1:448000);
+    thePacket.response.values = pupilSize + pupilMotor;
     
-    % In this mode, pupil response is proportional to the size at the
-    % beginning of stimulus presentation
-    pupilResponse(stimOnset) = neuronalResponse(stimOnset)/10 * pupilSize(stimOnset);
-    
-    
+    simulatedMergedPacketCellArray{1}{rr} = thePacket;
 end
 
-pupilMotor = conv(pupilResponse,pupilResponseFunction);
-pupilMotor = pupilMotor(1:448000);
-thePacket.response.values = pupilSize + pupilMotor;
-
-% for now, the low frequency component is set to 0
-thePacket.response.metaData.lowFreqComponent = zeros(1,448000);
-
-simulatedMergedPacketCellArray = [];
-simulatedMergedPacketCellArray{1} = []
-simulatedMergedPacketCellArray{1}{1} = thePacket;
-
+end
 
