7a95d58c792521de5d9257ae12f42c72a57bd6c9
harrison-mcadams <harrison.mcadams@gmail.com>
2016-10-25 11:52:53 -0400

    better commenting

--- loading/pupilPMEL_makeSimulatedData.m
+++ loading/pupilPMEL_makeSimulatedData.m
@@ -1,13 +1,9 @@
 function [simulatedMergedPacketCellArray] = pupilPMEL_makeSimulatedData()
 
 
-% Make stimulus vector. In this case, just grab an existing stimulus
-% vector
-% from some random subject.
 
-% Also make the neuronalResponse vector, which is log10(stimulus)+1 (+1 so
-% that 100% contrast and below gives positive signal)
 
+%% Load template data, in this case the cached version of our main data set
 
 % Here just stealing the code to load the packetCache to make it easy
 % Discover user name and set Dropbox path
@@ -51,28 +47,41 @@ switch packetCacheBehavior
         error('Please define a legal packetCacheBehavior');
 end
 
+
+%% Actually make the simulated data
+
+
+% make space for final simulated data
 simulatedMergedPacketCellArray = [];
-for rr = 1:(size(mergedPacketCellArray{1},2))
+for rr = 1:(size(mergedPacketCellArray{1},2)) % loop over runs
+    % the final simulated data will be based off all runs for session 1
     thePacket= mergedPacketCellArray{1}{rr};
     
     
+    % code used for translating stimulus type into contrast
     contrastList = {0.25; 0.50; 1; 2; 4; 8};
+    
+    % make basic structures for stimuli and response vectors 
     thePacket.stimulus.vector = zeros(1,length(thePacket.stimulus.timebase));
     neuronalResponse = zeros(1,length(thePacket.stimulus.timebase));
     pupilResponse = zeros(1,length(thePacket.stimulus.timebase));
     
     % create pupil response function, which is to convolved with the
     % neuronalResponse. For now, this pupil response function is just a step to
-    % 2 for 3 s
+    % 2 for 3 s. With a slight delay introduced so when later functions
+    % norm the time series for a given trial we can still do so
+    % appropriately.
     pupilResponseFunction = zeros(1,length(thePacket.stimulus.timebase));
     pupilResponseFunction(101:3000) = -2;
     
-    % Create pupil size vector. For now, just a baseline size of 5 mm
+    % Create pupil size vector. For now, the baseline pupil size is the loq
+    % frequency component for a given run.
     pupilSize = thePacket.response.metaData.lowFreqComponent;
     
-    
+    % pre-allocate space for data
     simulatedMergedPacketCellArray{1}{rr} = [];
-    for ii = 1:size(thePacket.stimulus.values,1);
+    
+    for ii = 1:size(thePacket.stimulus.values,1); % loop over instances
         
         % Find the stimulus onsets so that we can align the data to it. We
         % do that by finding a [0 1] edge from a difference operator.
@@ -89,7 +98,14 @@ for rr = 1:(size(mergedPacketCellArray{1},2))
         end
         contrast = thePacket.stimulus.metaData.stimTypes(ii);
         
+        % Make stimulus vector. In this case, just grab an existing stimulus
+        % vector
+        % from some random subject. contrastList converts from 1-6 stimuli
+        % designation to 0.25-0.5-1-etc.
         thePacket.stimulus.vector(stimOnset) = contrastList{contrast};
+        
+        % Make neuronalResponse vector. Simply encode stimulus contrast on
+        % a log scale
         neuronalResponse(stimOnset) = log10(thePacket.stimulus.vector(stimOnset))+1;
         
         % In this mode, pupil response is proportional to the size at the
@@ -99,10 +115,15 @@ for rr = 1:(size(mergedPacketCellArray{1},2))
         
     end
     
+    % make pupilMotor time series, which basically says how much we want to
+    % contract for a given stimulus
     pupilMotor = conv(pupilResponse,pupilResponseFunction);
     pupilMotor = pupilMotor(1:448000);
+    
+    % combine pupilMotor with pupilSize to get our pupil time series
     thePacket.response.values = pupilSize + pupilMotor;
     
+    % save in proper format
     simulatedMergedPacketCellArray{1}{rr} = thePacket;
 end
 
