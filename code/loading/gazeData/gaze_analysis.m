fe91769405d21cbca20ea2187fde06a0bd2f1853
Manuel Spitschan <spitschan@stanford.edu>
2017-03-06 16:30:43 -0800

    I save coupons for packets of tea
    Signed-off-by: Manuel Spitschan <spitschan@stanford.edu>

--- code/loading/gazeData/gaze_analysis.m
+++ code/loading/gazeData/gaze_analysis.m
@@ -9,13 +9,13 @@ for obs = 1:8
             'MelanopsinMRMaxLMS_PulseMaxLMS_3s_CRF16sSegment_50%' ...
             'MelanopsinMRMaxLMS_PulseMaxLMS_3s_CRF16sSegment_100%' ...
             'MelanopsinMRMaxLMS_PulseMaxLMS_3s_CRF16sSegment_200%' ...
-            'MelanopsinMRMaxLMS_PulseMaxLMS_3s_CRF16sSegment_400%'}
+            'MelanopsinMRMaxLMS_PulseMaxLMS_3s_CRF16sSegment_400%'};
     else
         theStimulusLabels = {'MelanopsinMRMaxMel_PulseMaxMel_3s_CRF16sSegment_25%' ...
             'MelanopsinMRMaxMel_PulseMaxMel_3s_CRF16sSegment_50%' ...
             'MelanopsinMRMaxMel_PulseMaxMel_3s_CRF16sSegment_100%' ...
             'MelanopsinMRMaxMel_PulseMaxMel_3s_CRF16sSegment_200%' ...
-            'MelanopsinMRMaxMel_PulseMaxMel_3s_CRF16sSegment_400%'}
+            'MelanopsinMRMaxMel_PulseMaxMel_3s_CRF16sSegment_400%'};
     end
     
     % Populate an empty vector
@@ -38,16 +38,15 @@ for obs = 1:8
         for mm = 1:size(mergedPacketCellArray{obs}{ii}.stimulus.values, 1)
             idx_stimOn = find(mergedPacketCellArray{obs}{ii}.stimulus.values(mm, :) == 1);
             idx_stimOff = idx_stimOn(end)+4000:idx_stimOn(end)+6000;
-            gazeX_theVarOn(ii, mm) = var(mergedPacketCellArray{obs}{ii}.response.gazeX(idx_stimOn));
-            gazeX_theVarOff(ii, mm) = var(mergedPacketCellArray{obs}{ii}.response.gazeX(idx_stimOff));
-            gazeY_theVarOn(ii, mm) = var(mergedPacketCellArray{obs}{ii}.response.gazeY(idx_stimOn));
-            gazeY_theVarOff(ii, mm) = var(mergedPacketCellArray{obs}{ii}.response.gazeY(idx_stimOff));
+            gazeX_theVarOn(ii, mm) = mean(mergedPacketCellArray{obs}{ii}.response.gazeX(idx_stimOn));
+            gazeX_theVarOff(ii, mm) = mean(mergedPacketCellArray{obs}{ii}.response.gazeX(idx_stimOff));
+            gazeY_theVarOn(ii, mm) = mean(mergedPacketCellArray{obs}{ii}.response.gazeY(idx_stimOn));
+            gazeY_theVarOff(ii, mm) = mean(mergedPacketCellArray{obs}{ii}.response.gazeY(idx_stimOff));
         end
         
         % Iterate over the stimulus types
         for kk = 1:NStimuli
-            theStimulusLabels{kk}
-            theIdx = find(ismember(allStimLabels, theStimulusLabels{kk}))
+            theIdx = find(ismember(allStimLabels, theStimulusLabels{kk}));
             gazeX_aggregateVarOn{kk} = [gazeX_aggregateVarOn{kk} gazeX_theVarOn(ii, theIdx)];
             gazeX_aggregateVarOff{kk} = [gazeX_aggregateVarOff{kk} gazeX_theVarOff(ii, theIdx)];
             gazeY_aggregateVarOn{kk} = [gazeY_aggregateVarOn{kk} gazeY_theVarOn(ii, theIdx)];
@@ -56,20 +55,38 @@ for obs = 1:8
     end
     
     % Plot the data
+    fprintf('\n');
+    fileName = [mergedPacketCellArray{obs}{1}.metaData.projectName '_' mergedPacketCellArray{obs}{1}.metaData.subjectName '_' mergedPacketCellArray{obs}{1}.metaData.sessionDate];
+
     figure;
     subplot(1, 2, 1);
     for kk = 1:NStimuli
-        errorbar(kk-0.1, mean(gazeX_aggregateVarOff{kk}), std(gazeX_aggregateVarOff{kk}), 'sk', 'MarkerFaceColor', 'k'); hold on;
-        errorbar(kk+0.1, mean(gazeX_aggregateVarOn{kk}), std(gazeX_aggregateVarOn{kk}), 'sr', 'MarkerFaceColor', 'r');
+        fprintf('Gaze X (off-stimulus): Stim %i, Mean %f SD %f\n', kk, nanmean(gazeX_aggregateVarOff{kk}), nanstd(gazeX_aggregateVarOff{kk}));
+        fprintf('Gaze X (on-stimulus): Stim %i, Mean %f SD %f\n', kk, nanmean(gazeX_aggregateVarOn{kk}), nanstd(gazeX_aggregateVarOn{kk}));
+        errorbar(kk-0.1, nanmean(gazeX_aggregateVarOff{kk}), nanstd(gazeX_aggregateVarOff{kk}), 'sk', 'MarkerFaceColor', 'k'); hold on;
+        errorbar(kk+0.1, nanmean(gazeX_aggregateVarOn{kk}), nanstd(gazeX_aggregateVarOn{kk}), 'sr', 'MarkerFaceColor', 'r');
     end
+    set(gca, 'XTick', 1:NStimuli, 'XTickLabel', {'25%', '50%', '100%', '200%', '400%'});
     ylabel('X variance'); xlabel('Stimulus contrast');
     pbaspect([1 1 1]);
+    set(gca, 'TickDir', 'out');  box off;
+    title(strrep(fileName, '_', ' '));
     
     subplot(1, 2, 2);
     for kk = 1:NStimuli
-        errorbar(kk-0.1, mean(gazeY_aggregateVarOff{kk}), std(gazeY_aggregateVarOff{kk}), 'sk', 'MarkerFaceColor', 'k'); hold on;
-        errorbar(kk+0.1, mean(gazeY_aggregateVarOn{kk}), std(gazeY_aggregateVarOn{kk}), 'sr', 'MarkerFaceColor', 'r');
+        fprintf('Gaze Y (off-stimulus): Stim %i, Mean %f SD %f\n', kk, nanmean(gazeY_aggregateVarOff{kk}), nanstd(gazeY_aggregateVarOff{kk}));
+        fprintf('Gaze Y (on-stimulus): Stim %i, Mean %f SD %f\n', kk, nanmean(gazeY_aggregateVarOn{kk}), nanstd(gazeY_aggregateVarOn{kk}));
+        errorbar(kk-0.1, nanmean(gazeY_aggregateVarOff{kk}), nanstd(gazeY_aggregateVarOff{kk}), 'sk', 'MarkerFaceColor', 'k'); hold on;
+        errorbar(kk+0.1, nanmean(gazeY_aggregateVarOn{kk}), nanstd(gazeY_aggregateVarOn{kk}), 'sr', 'MarkerFaceColor', 'r');
     end
+    set(gca, 'XTick', 1:NStimuli, 'XTickLabel', {'25%', '50%', '100%', '200%', '400%'});
     ylabel('Y variance'); xlabel('Stimulus contrast');
     pbaspect([1 1 1]);
+    set(gca, 'TickDir', 'out'); box off;
+    title(strrep(fileName, '_', ' '));
+    
+    set(gcf, 'PaperPosition', [0 0 8 4]);
+    set(gcf, 'PaperSize', [8 4]);
+    saveas(gcf, [fileName '.png'], 'png');
+    close(gcf);
 end
--- code/loading/pupilPMEL_assembleEyeTrackingData.m
+++ code/loading/pupilPMEL_assembleEyeTrackingData.m
@@ -14,9 +14,9 @@ dropboxAnalysisDir = ...
 % Define packetCacheBehavior. Options include:
 %    'make' - load and process stim/response files, save the packets
 %    'load' - load the packets from the passed hash name
-packetCacheBehavior='make';
+packetCacheBehavior='load';
 packetCellArrayTag='maxMelLMS_CRF_Pupil';
-packetCellArrayHash='d702e5898732600707fb32a2302d4772';
+packetCellArrayHash='022b1d8d9d3a3cfe939c923d830601f8';
 
 %% Create or load the packetCellArray
 switch packetCacheBehavior
